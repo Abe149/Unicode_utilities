2ff368312850dd64de5bd9e1b72b09e4fa540b98
Abe <abe149@gmail.com>
2022-05-07 15:54:00 -0400

    “ASCII_to_Unicode_Japanese.py”: added 1 leading space char. in a place where it was not a syntax error to not have it, but its missingness _did_ cause that line [in a/the primary “if” block] to fail to line up nicely with code in the related “elif” block

--- ASCII_to_Unicode_Japanese.py
+++ ASCII_to_Unicode_Japanese.py
@@ -5,7 +5,7 @@ for line in stdin.readlines():
   for char in line:
     o = ord(char)
     if 32 == o: ### ASCII SPACE
-     stdout.write(chr(0x3000)) ### IDEOGRAPHIC SPACE
+      stdout.write(chr(0x3000)) ### IDEOGRAPHIC SPACE
     elif 33 <= o <= 127:
       stdout.write(chr(o+0xFF01-33))
     else:
